015eba738366d65cb777c1f325a63f27054a42ca
Nathan Harper <nathan.sam.harper@gmail.com>
2025-06-11 03:57:02 -0400

    parse url

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,30 @@
+import { URL } from 'node:url';
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'story_plotter',
-  password: process.env.DB_PASSWORD || 'password',
-  port: parseInt(process.env.DB_PORT || '5432'),
-});
+let pool: Pool;
+
+if (process.env.DATABASE_URL) {
+  const params = new URL(process.env.DATABASE_URL);
+
+const config = {
+  user: params.username,
+  password: params.password,
+  host: params.hostname,
+  port: parseInt(params.port),
+  database: params.pathname.split('/')[1],
+};
+
+  pool = new Pool(config);
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'story_plotter',
+    password: process.env.DB_PASSWORD || 'password',
+    port: parseInt(process.env.DB_PORT || '5432'),
+  });
+}
+
 
 export interface Character {
   id: number;
